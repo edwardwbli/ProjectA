a73a9f9d9c4f3ab640bab9f8c465313e3522dd28
edwardwbli <edward.w.b.li@icloud.com>
2016-08-10 09:51:24 +0800

    Call function in specifid interval
    Add function snap on how to call function in specifid interval

--- jQeurysnap.js
+++ jQeurysnap.js
@@ -1,4 +1,5 @@
 jQeury snap
+
 #Auto detect input and insert input value in #result tag.
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.3.min.js" ></script>
 <script type="text/javascript">
@@ -12,8 +13,16 @@ jQeury snap
 <div id="result">
 </div>
 ###
+
 #jQuery Get web page, and insert response into  tag with class .result.
 $.get( "ajax/test.html", function( data ) {
   $( ".result" ).html( data );
   alert( "Load was performed." );
 });
+
+#invoke api on specified interval
+var ajax_call = function() {
+  //your jQuery ajax code
+};
+var interval = 1000 * 60 * X; // where X is your every X minutes
+setInterval(ajax_call, interval);
